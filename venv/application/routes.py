b6daf401eccdce07c82761def38be764d1f685f5
RushikeshTadvalkar <rushikesh.tadwalkar@gmail.com>
2020-10-22 01:19:05 +0530

    initial commit

---
+++ venv/application/routes.py
@@ -0,0 +1,11 @@
+from application import app
+from flask import render_template
+
+@app.route("/")
+@app.route("/index")
+def index():
+    return render_template("index.html")
+    """
+    docstring
+    """
+    pass
